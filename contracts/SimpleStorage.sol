b7c7fcbf25fadd32cbde9ffdcedf25e27f5234f2
Remix Workspace <>
2022-05-21 14:16:37 -0600

    Initial commit.

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.8.7;
+
+contract SimpleStorage {
+   uint256 favoriteNumber;
+   // 0
+
+   struct Person {
+       uint256 favoriteNumber;
+       string name;
+   }
+
+   Person[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(Person({favoriteNumber: _favoriteNumber, name: _name}));
+    }
+}
